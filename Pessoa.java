60dd14161249fa6921636c5482798be5a732aec0
exercio-java-poo <victorbaraldi@gmail.com>
2025-08-22 16:05:52 -0300

    enviando o arquivo teste

---
+++ Pessoa.java
@@ -0,0 +1,19 @@
+
+public class Pessoa {
+
+    String nome;
+    int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void Apresentar() {
+        System.out.println("Olá, meu nome é " + nome + " e tenho " + idade + " anos.");
+    }
+
+    public boolean maiorDeIdade() {
+        return idade >= 18;
+    }
+}
