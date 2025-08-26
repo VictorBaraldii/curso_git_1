b06766b9283ba89516737e423cd026cc0799da05
Victor Baraldi <victorbaraldi3@gmail.com>
2025-08-26 13:38:34 -0300

    Update about.java

--- about.java
+++ about.java
@@ -1,9 +1,7 @@
 public class Teste() {
 
-  public static void main(String[] args)
-    {
+  public static void main(String[] args) {
 
-    System.ou.println("Teste gitHub")
-  }
-  
+    System.ou.println("Teste gitHub");
     }
+  }
